f920682dc19b601c55d3cd7d533e3a72ed56e0e8
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-02-20 13:51:47 -0300

    estudos em java

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //maneira de adicionar elementos sem definir o tamanho
+        ArrayList<String> nomes = new ArrayList<>();
+        nomes.add("Pedro");
+        System.out.println(nomes.get(0));
+
+        //loop for in carro
+        String[] carro = {"ford", "ferrari", "pagani"};
+        for (String i : carro) {
+            System.out.println(i);
+        }
+
+        //maneira de definir o tamanho do array
+        int[] notas = new int[10];
+        for (int i : notas){
+            notas[i]+=1;
+            System.out.println(notas[i]);
+        }
+    }
+}
