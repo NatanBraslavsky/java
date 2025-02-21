4b8c150f783b90b818bb3a3c5cfed1bc901c5bb6
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-02-20 21:30:11 -0300

    aula

--- faculdade/exercicios/fase1/src/controller/Main.java
+++ faculdade/exercicios/fase1/src/controller/Main.java
@@ -1,11 +1,13 @@
 package controller;
 import java.util.Scanner;
 
+import static java.lang.Math.sqrt;
+
 public class Main {
     public static void main(String[] args)
     {
      Scanner sc = new Scanner(System.in);
-//     Ex 001:
+//     ?Ex 001:
 //     System.out.println("Digite um número:");
 //     int num1 = sc.nextInt();
 //     System.out.println("Digite outro número:");
@@ -13,21 +15,84 @@ public class Main {
 //     int soma = num1 + num2;
 //     System.out.printf("A soma entre %d e %d = %d", num1, num2, soma);
 
-//      Ex 002:
+//      ?Ex 002:
 //      System.out.printf("Digite o raio do circulo: ");
 //      float raio = sc.nextFloat();
 //      float area = (float) (Math.PI * Math.pow(raio, 2));
 //      System.out.printf("A area do circulo é: %.2f", area);
 
-//      Ex 003:
+//      ?Ex 003:
 //        Fórmula: F = (C × 9/5) + 32.
 //        System.out.printf("Digite a temperatura em Celsius: ");
 //        float celsius = sc.nextFloat();
 //        float fahrenheit = (celsius * 9/5) + 32;
 //        System.out.printf("%.2f Celsius em Fahrenheit: %.2f", celsius, fahrenheit);
 
-        
+//        ?Ex 004
+//        float[] notas = new float[3];
+//        float soma = 0;
+//        for (int i = 0; i < 3; i++){
+//            System.out.printf("Digite uma nota: ");
+//            notas[i] = sc.nextFloat();
+//            soma += notas[i];
+//        }
+//        float media = soma / 3;
+//        System.out.printf("Media: %.2f", media);
+
+//        ?Ex 005
+//        System.out.printf("Digite sua idade: ");
+//        int idade = sc.nextInt();
+//        int dias = idade * 365;
+//        System.out.printf("Voce viveu %d dias.", dias);
+
+//        ?Ex 006
+//        System.out.printf("Digite seu salario: ");
+//        float salario = sc.nextFloat();
+//        System.out.printf("Digite o valor do desconto: ");
+//        float desconto = sc.nextFloat();
+//        desconto = desconto / 100;
+//        System.out.printf("Seu salario ficou: R$%.2f", salario - salario * desconto);
+
+//        ?Ex 007
+//        System.out.printf("Digite a base do retangulo: ");
+//        float base = sc.nextFloat();
+//        System.out.printf("Digite a altura do retangulo: ");
+//        float altura = sc.nextFloat();
+//        float area = base * altura;
+//        float perimetro = (base + altura) * 2;
+//        System.out.printf("A area é: %.2f \nO perímetro é: %.2f", area, perimetro);
 
 
+//        ?Ex 008
+//        System.out.printf("Digite o raio da esfera: ");
+//        double raio = sc.nextDouble();
+//        double v = (4/3) * Math.PI * Math.pow(raio,3);
+//        System.out.printf("Volume: %.2f", v);
+
+
+//        ?Ex 009
+//        System.out.printf("Digite o valor em reais R$");
+//        double reais = sc.nextDouble();
+//        System.out.printf("Digite a cotação do dolar $");
+//        double cotacao = sc.nextDouble();
+//        System.out.printf("Com R$%.2f você possui $%.2f", reais, reais/cotacao);
+
+//        ?Ex 010
+//        double[] x = new double[2];
+//        double[] y = new double[2];
+//        for(int i = 0; i < 2; i++){
+//            System.out.printf("Digite a cordenada x %d", i+1);
+//            x[i] = sc.nextDouble();
+//        }
+//        for(int i = 0; i < 2; i++){
+//            System.out.printf("Digite a cordenada y %d", i+1);
+//            y[i] = sc.nextDouble();
+//        }
+//        double ymenos = y[1] - y[0];
+//        double powymenos = Math.pow(ymenos, 2);
+//        double xmenos = x[1] - x[0];
+//        double powxmenos = Math.pow(xmenos, 2);
+//        double sqrtxmenos = sqrt(powxmenos + powymenos);
+//        System.out.printf("Distancia: %.2f", sqrtxmenos);
     }
 }
