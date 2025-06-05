2507ebb46c8bd98e1848c03fe2f6039aa680a38d
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-06-05 14:45:43 -0300

    mini projeto teste em java

--- projetos/selecionar_dupla/src/Main.java
+++ projetos/selecionar_dupla/src/Main.java
@@ -1,5 +1,33 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ArrayList<String> nomes = new ArrayList<>();
+        String caminho = "src/nomes.txt";
+        Random random = new Random();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(caminho))){
+         String linha;
+         while((linha = reader.readLine()) != null){
+             nomes.add(linha);
+         }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+        if (!nomes.isEmpty()) {
+            String nomeSorteado = nomeAleatorio(nomes, random);
+            System.out.println("Nome sorteado: " + nomeSorteado);
+        } else {
+            System.out.println("Lista de nomes está vazia.");
+        }
+    }
+
+    public static String nomeAleatorio(ArrayList<String> nomes, Random random){
+        int indiceAleatorio = random.nextInt(nomes.size());
+        return nomes.get(indiceAleatorio);
     }
 }
