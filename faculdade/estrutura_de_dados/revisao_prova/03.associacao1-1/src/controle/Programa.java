470ca1ec5f2399ff7b4f208219eb6bfe576019f4
NatanBraslavsky <natanbraslavsky1@gmail.com>
2025-10-07 14:42:58 -0300

    Revisao prova associação unaria

---
+++ faculdade/estrutura_de_dados/revisao_prova/03.associacao1-1/src/controle/Programa.java
@@ -0,0 +1,32 @@
+package controle;
+
+import java.util.Scanner;
+import modelo.Gerente;
+import modelo.Setor;
+
+
+
+public class Programa {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("CADASTRO DO SETOR: ");
+        System.out.print("Digite a sigla: ");
+        String sigla = sc.nextLine();
+        System.out.print("Digite a descrição: ");
+        String descricao = sc.nextLine();
+
+        System.out.println("CADASTRO DO GERENTE");
+        System.out.print("Digite o CPF: ");
+        String cpf = sc.nextLine();
+        System.out.print("Digite o nome: ");
+        String nome = sc.nextLine();
+
+        Setor setor = new Setor(sigla, descricao);
+        Gerente gerente = new Gerente(cpf, nome);
+        setor.setGerente(gerente);
+        gerente.setSetor(setor);
+
+        System.out.println("O funcionário " + setor.getGerente().getNome() + " é gerente do setor: " + gerente.getSetor().getDescricao());
+    }
+}
